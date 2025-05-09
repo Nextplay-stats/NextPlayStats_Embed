a2186ca2065171c021b5de322b6ed27555078bc8
Nextplay-stats <nextplaystats@outlook.com>
2025-05-09 15:40:33 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,18 @@
+const msalConfig = {
+  auth: {
+    clientId: "YOUR_CLIENT_ID",
+    authority: "https://login.microsoftonline.com/YOUR_TENANT_ID",
+    redirectUri: "https://yourusername.github.io/repo-name",
+  }
+};
 
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+function signIn() {
+  msalInstance.loginPopup({ scopes: ["user.read"] })
+    .then(response => {
+      console.log("Login successful", response);
+      // Check user level here
+    })
+    .catch(error => console.error(error));
+}
